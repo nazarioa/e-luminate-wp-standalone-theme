585c4751e75cd2088655a80c9b6bf6f9137b0888
Nazario A. Ayala <nazario@niztech.com>
2026-02-04 11:21:53 -0800

    Added front-page

---
+++ front-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Default template index.php.
+ *
+ * Default template index.php.
+ *
+ * @category   Theme
+ * @package eluminate-standalone
+ * @author     Nazario A. Ayala <nazario@niztech.com>
+ * @license    opensource.org MIT License
+ * @link       https://www.niztech.com
+ * @since      0.0.1
+ */
+
+get_template_part( 'template-parts/layout', 'start', array( 'class' => array( 'front-page' ) ) );
+get_template_part( 'template-parts/header' );
+get_template_part( 'template-parts/layout', 'nav' );
+get_template_part( 'template-parts/main', 'start' );
+?>
+	<h2 class="main-title"><?php the_title(); ?></h2>
+<?php
+
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		global $post;
+		the_post();
+		the_content();
+	endwhile;
+else :
+	get_template_part( 'template-parts/404' );
+endif;
+
+get_template_part( 'template-parts/main', 'end' );
+get_template_part( 'template-parts/footer' );
+get_template_part( 'template-parts/layout', 'end' );
